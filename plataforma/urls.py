69d2d29d7f79917a4794666738f467eefbe86aef
Wilder <wilder_esc96@hotmail.com>
2018-09-12 00:56:43 -0500

    iniciando

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
     path('', include('unipymes.urls')),
+    path('', include('pwa.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
