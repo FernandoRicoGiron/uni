be906dbf202776bce09906108e8b431d088ca974
Rico <riicoo28@gmail.com>
2018-11-04 17:14:59 -0600

    fotos en quienes somos unipymes

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'unipymes',
-        'USER': 'postgres',
+        'USER': 'unipymes',
         'PASSWORD': 'rockmax28',
         'HOST': 'localhost',
         'PORT': ''
